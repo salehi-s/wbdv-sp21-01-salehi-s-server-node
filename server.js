fd3a52af8afc6f054cc688b9847fef6520b8dedb
salehi-s <salehi.s@husky.neu.edu>
2021-04-11 19:04:26 -0400

    Update Server with CORS Configuration

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const app = express()
 
-const quizzesController = require("./controllers/quizzes-controller")
-quizzesController(app)
+// Configure CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Origin")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+    next()
+})
 
-const questionsController = require("./controllers/questions-controller")
-questionsController(app)
+
+require("./controllers/quizzes-controller")(app)
+
+require("./controllers/questions-controller")(app)
 
 app.listen(3000)
