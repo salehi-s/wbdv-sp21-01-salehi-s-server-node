f47b158115a0e44645d202d21de65fcdac59b84d
salehi-s <salehi.s@husky.neu.edu>
2021-04-11 23:55:51 -0400

    Implement CRUD Operation Functions in Quizzes Service and Questions Service

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -1,13 +1,20 @@
-const questionsService = require("../services/questions-service")
+const questionsService = require("../services/questions/questions-service")
 
 module.exports = (app) => {
 
+    const findAllQuestions = (req, res) => {
+        const questions = questionsService.findAllQuestions()
+        res.send(questions)
+    }
+
     const findQuestionsForQuiz = (req, res) => {
-        const quizId = req.params["qid"]
+        const quizId = req.params["quizId"]
         const questions = questionsService.findQuestionsForQuiz(quizId)
         res.send(questions)
     }
 
-    app.get("/api/quizzes/:qid/questions", findQuestionsForQuiz)
+    app.get("/api/questions", findAllQuestions)
+
+    app.get("/api/quizzes/:quizId/questions", findQuestionsForQuiz)
 
 }
