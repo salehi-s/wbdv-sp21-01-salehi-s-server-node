88bd921ecce5bd49e67751fd60df714cf23cb173
salehi-s <salehi.s@husky.neu.edu>
2021-04-20 22:24:02 -0400

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-// const uri = process.env.MONGODB_URI
+// Remote Implementation
+const uri = process.env.MONGODB_URI
 
 // Local Implementation
-const uri = "mongodb://localhost:27017/whiteboard"
+// const uri = "mongodb://localhost:27017/whiteboard"
 
 const express = require("express")
 const app = express()
